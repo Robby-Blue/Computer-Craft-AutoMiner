1d5d9b2cfb158a361e38b75fd2af2ac011ed7d04
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2021-02-03 18:33:19 +0100

    Add files via upload

---
+++ src/universal-installer.lua
@@ -0,0 +1,48 @@
+function install(link, file)
+    local request = http.get(link)
+    local startupcode = request.readAll()
+    request.close()
+
+    local startupfile = fs.open(file,"w")
+    startupfile.write(code)
+    startupfile.close()
+end
+
+local win = window.create(term.current(), 1, 1, term.getSize())
+
+local width, height = term.getSize()
+local stop = false
+local type
+
+win.setBackgroundColor(colors.blue)
+win.clear()
+win.setCursorPos(width / 2 - (string.len("Server") / 2), height / 2 )
+win.write("Server")
+win.setCursorPos(width / 2 - (string.len("Client") / 2), height / 2 + 1)
+win.write("Client")
+
+while not stop do
+    local event, button, x, y = os.pullEvent("mouse_click")
+    if x > width / 2 - (string.len("Server") / 2) and x < width / 2 + (string.len("Server") / 2) and y == height / 2 then
+        stop = true
+        type = "Server"
+    end
+    if x > width / 2 - (string.len("Client") / 2) and x < width / 2 + (string.len("Client") / 2) and y == height / 2 + 1 then
+        stop = true
+        type = "Client"
+    end
+end
+
+win.clear()
+win.setCursorPos(width / 2 - (string.len("Downloading files") / 2), height / 2 )
+win.write("Downloading files")
+
+if type == "Server" then
+    install("https://raw.githubusercontent.com/Robby-Blue/ComputerCraftStuff/main/Server/startup.lua", "startup.lua")
+    install("https://raw.githubusercontent.com/Robby-Blue/ComputerCraftStuff/main/Server/server.lua", "server.lua")
+end
+
+if type == "Client" then
+    install("https://raw.githubusercontent.com/Robby-Blue/ComputerCraftStuff/main/Client/startup.lua", "startup.lua")
+    install("https://raw.githubusercontent.com/Robby-Blue/ComputerCraftStuff/main/Client/client.lua", "client.lua")
+end
