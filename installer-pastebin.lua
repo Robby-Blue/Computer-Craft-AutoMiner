eab39a9d652e38ef932dc1b3848e7a3b860624ac
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2021-02-03 18:01:46 +0100

    Create installer-pastebin.lua

---
+++ installer-pastebin.lua
@@ -0,0 +1,14 @@
+-- code pasted on pastebin allowing for easy install
+
+local request = http.get("https://raw.githubusercontent.com/Robby-Blue/Computer-Craft-AutoMiner/main/installer.lua")
+local code = request.readAll()
+request.close()
+ 
+local file = fs.open("installing.lua","w")
+file.write(code)
+file.close()
+ 
+shell.run("installing.lua")
+sleep(1)
+fs.delete("installing.lua")
+os.reboot()
