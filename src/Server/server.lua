a0a3d2bc6a8a5c3f6a019cf9a81c5de809f4fab0
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2021-02-03 22:51:28 +0100

    better gui

--- src/Server/server.lua
+++ src/Server/server.lua
@@ -3,7 +3,7 @@ local win = window.create(term.current(), 1, 1, term.getSize())
 local width, height = term.getSize()
 local screen = "start"
 
-local clients = {}
+local clients = {{}}
 
 function drawScreen()
     win.setBackgroundColor(colors.blue)
@@ -13,9 +13,11 @@ function drawScreen()
         win.write("Click anywhere to start")
     end
     if screen == "connect" then
+        win.setCursorPos(1,1)
+        win.write("Connected clients")
         for i = 1, table.getn(clients) do
-            win.setCursorPos(2, i + 1)
-            win.write(clients[i])
+            win.setCursorPos(2, i + 2)
+            win.write("Computer ID: "..clients[i][1].." Personal ID: "..clients[i][2])
         end
     end
 end
@@ -27,6 +29,15 @@ function startServer()
     end
 end
 
+function getFacingDirection()
+    if not turtle.detect() then
+        local x1, y1, z1 = gps.locate()
+        turtle.forward()
+        local x2, y2, z2 = gps.locate()
+        turtle.back()
+    end
+end
+
 function mouseClick()
     while true do
         local event, button, x, y = os.pullEvent("mouse_click")
@@ -34,6 +45,8 @@ function mouseClick()
         if screen == "start" then
             screen = "connect"
             rednet.broadcast("autominer.start")
+            sleep(1)
+            screen = "move"
         end
     end
 end
