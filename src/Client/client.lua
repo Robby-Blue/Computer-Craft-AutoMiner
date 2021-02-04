5513250adccea46114b94f9f009275c3cc3bd5be
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2021-02-04 14:09:26 +0100

    variable

--- src/Client/client.lua
+++ src/Client/client.lua
@@ -119,7 +119,7 @@ function gotoAxis(axis, coords)
         elseif coordx < coords then
             turn(1)
         end
-        while coordxy ~= coords do
+        while coordx ~= coords do
             moveForward()
         end
     end
