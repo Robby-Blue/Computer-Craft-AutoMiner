8148bfffe14fed7c3ccbc0ab1d45ad112ec1f303
Robby_Blue <69482496+Robby-Blue@users.noreply.github.com>
2021-02-04 23:16:14 +0100

    new now working installer

--- src/pastebin-installer.lua
+++ src/pastebin-installer.lua
@@ -12,16 +12,17 @@ function runInstaller(link)
     shell.run("installing.lua")
     sleep(1)
     fs.delete("installing.lua")
+    os.reboot()
 end
 
 local args = { ... }
 
 if table.getn(args) == 0 then
     runInstaller("https://raw.githubusercontent.com/Robby-Blue/Computer-Craft-AutoMiner/main/src/universal-installer.lua")
-elseif table[1] == "client" or table[1] == "Client" then
+elseif args[1] == "client" or table[1] == "Client" then
     runInstaller("https://raw.githubusercontent.com/Robby-Blue/Computer-Craft-AutoMiner/main/src/Client/client-installer.lua")
-elseif table[1] == "server" or table[1] == "Server" then
+elseif args[1] == "server" or table[1] == "Server" then
     runInstaller("https://raw.githubusercontent.com/Robby-Blue/Computer-Craft-AutoMiner/main/src/Server/server-installer.lua")
+else
+    print("Invalid argument")
 end
-
-os.reboot()
